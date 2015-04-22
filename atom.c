b38a381bc254a9e1946bf30c72e4b703853db1d3
jedi453 <dansecure1@gmail.com>
2015-04-22 11:39:44 -0400

    Added Murmur3 Hash, abstractions for hash testing, testing for Murmur2+3, FNV-1a and Lose

--- atom.c
+++ atom.c
@@ -20,9 +20,10 @@
 #if 0
 #define ATOM_BUCKETS 2048
 #define ATOM_BUCKETS 2039
+#define ATOM_BUCKETS 4096
 #define ATOM_BUCKETS 4093
 #endif
-#define ATOM_BUCKETS 4096
+#define ATOM_BUCKETS 2048
 /* MACROS - END */
 
 
--- hash.c
+++ hash.c
@@ -3,13 +3,30 @@
 #include "assert.h"
 #include <limits.h>
 
+HASH_TYPE Hash_FNV1A(const char*, size_t);
+HASH_TYPE Hash_LOSE(const char*, size_t);
+unsigned int Hash_MURMUR2(const char*, size_t);
+unsigned int Hash_MURMUR3(const char*, size_t, unsigned int);
 
+#define MURMUR3_SEED 0x93C467E3
+
+
+HASH_TYPE doHash(const char* str, size_t len) {
 #if 0
+  return Hash_LOSE(str, len);
+  return Hash_MURMUR2(str, len);
+  return Hash_MURMUR3(str, len, MURMUR3_SEED);
+#endif
+  return Hash_FNV1A(str, len);
+}
+
+
+
 /* Functions - BEGIN */
 /* FNV-1A Hash on Given String of Given Length 
  *  DON'T EXPOSE TO CLIENT! DON'T PUT IN atom.h
  */
-HASH_TYPE doHash( const char *str, size_t len ) {
+HASH_TYPE Hash_FNV1A( const char *str, size_t len ) {
   HASH_TYPE hash = 0x811C9DC5UL;
   HASH_TYPE prime = 0x01000193UL;
   unsigned char buf;
@@ -32,11 +49,9 @@ HASH_TYPE doHash( const char *str, size_t len ) {
 
   return hash;
 }
-#endif
 
-#if 0
 /** Lose-Lose - FOR TESTING ONLY - VERY SUCK */
-HASH_TYPE doHash( const char *str, size_t len ) {
+HASH_TYPE Hash_LOSE( const char *str, size_t len ) {
   size_t i;
   HASH_TYPE hash = 0;
 
@@ -45,7 +60,7 @@ HASH_TYPE doHash( const char *str, size_t len ) {
 
   return hash;
 }
-#endif
+
 
 /****************** BEGIN Murmur2 Hash *******************/
 /** Murmur 2 Mix Constants that Work Well, Thanks to Austin Appleby */
@@ -59,7 +74,7 @@ HASH_TYPE doHash( const char *str, size_t len ) {
 #define MURMUR2_SEED  0x93C467E3u
 
 
-unsigned int doHash( const char* str, size_t len )
+unsigned int Hash_MURMUR2( const char* str, size_t len )
 {
   assert(sizeof(unsigned int)*CHAR_BIT == 32);
   unsigned int hash;
@@ -104,3 +119,72 @@ unsigned int doHash( const char* str, size_t len )
   return hash;
 }
 /****************** END Murmur2 Hash *******************/
+
+
+
+
+/****************** BEGIN Murmur3 Hash *******************/
+unsigned int Hash_MURMUR3(const char* str, size_t len, unsigned int seed) {
+  unsigned int c1 =   0xCC9E2D51U;
+  unsigned int c2 =   0x1B873593U;
+  unsigned char r1 =  15u;
+  unsigned char r2 =  13u;
+  unsigned int m =    5u;
+  unsigned int n =    0xE6546B64U;
+  unsigned int hash = seed;
+  const unsigned int* data;
+  unsigned int tmp;  /* Temporary Holder for Intermediate Hashing Results */
+  size_t wlen = len; /* The Working Length, For Main Loop, Full len Used Later*/
+
+  /* Make Some (Probably Good) Assumptions About Integer Types,
+   *  to Avoid Needing stdint.h and C99
+   */
+  assert(CHAR_BIT == 8);
+  assert((sizeof(unsigned int)/sizeof(unsigned char)) == 4);
+
+  /* Begin Hashing */
+  for( data = (const unsigned int*)str; wlen >= 4; wlen -= 4, ++data ) {
+    tmp = *data;
+
+    tmp *= c1;
+    tmp = (tmp << r1) | (tmp >> (32-r1)); /* Rotate tmp by r1 to Left */
+    tmp *= c2;
+
+    hash ^= tmp;
+    hash = (hash << r2) | (hash >> (32-r2));
+    hash = hash*m + n;
+  }
+
+  /* Reverse Byte order of Remaining Bytes
+   *  Then Perform Regular Mix on it
+   */
+  tmp = 0u;
+  switch (wlen) {
+    case 3:
+      tmp ^= (0x0000FF00u & *data) << 8;
+    case 2:
+      tmp ^= (0x00FF0000u & *data) >> 8;
+    case 1:
+      tmp ^= (0xFF000000u & *data) >> 24;
+
+      /* Same Mix as Main Loop */
+      tmp *= c1;
+      tmp = (tmp<<r1) | (tmp>>(32-r1));
+      tmp *= c2;
+
+      hash ^= tmp;
+      break;
+  }
+
+  /* Final Mixing */
+  hash ^= len;
+  hash ^= (hash >> 16);
+  hash *= 0x85EBCA6B;
+  hash ^= (hash >> 13);
+  hash *= 0xC2B2AE35;
+  hash ^= (hash >> 16);
+
+  return hash;
+}
+
+/****************** END Murmur3 Hash *******************/
